8680b0ac3c5827ae271d7b682763a7f47f10879c
gabrielsimongianotti <gabrielgianotti@outlook.com>
2019-04-17 12:02:04 -0300

    aula 18

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,26 +1,49 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
-exports.post =(req,res,next) =>{
+exports.get = (req, res, next) => {
+    Product
+        //    active so traz  apelios indicados
+        .find({active: true}, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+exports.getBySlug = (req, res, next) => {
+    Product
+        //    active so traz  apelios indicados
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+exports.post = (req, res, next) => {
     console.log(req.body)
     var product = new Product(req.body);
-    
+
     product
-    .save()
-    .then(x=>{
-        res.status(201).send({
-            message:'preduto cadastrado com sucesso!',
-            data:x
+        .save()
+        .then(x => {
+            res.status(201).send({
+                message: 'preduto cadastrado com sucesso!',
+                data: x
+            });
+        }).catch(e => {
+            res.status(400).send({
+                messagem: 'falha ap cadastrar o produto',
+                data: e
+            })
         });
-    }).catch(e=>{
-        res.status(400).send({
-            messagem:'falha ap cadastrar o produto',
-            data:e
-        })
-    });
-    
+
 };
 
-exports.put =(req,res,next) =>{
+exports.put = (req, res, next) => {
     let id = req.params.id;
     res.status(201).send({
         id: id,
@@ -28,6 +51,6 @@ exports.put =(req,res,next) =>{
     });
 };
 
-exports.delete =(req,res,next) =>{
+exports.delete = (req, res, next) => {
     res.status(200).send(req.body);
 };
--- src/route/product-route.js
+++ src/route/product-route.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/product-controller');
 router.post('/', controller.post);
 router.put('/:id',controller.put);
 router.delete('/', controller.delete);
-
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
 module.exports= router;
